26c0da6fbf34055693eb0257cebe3902bf7ecd75
MATU_LEGUI <92628713+Matulegui201297@users.noreply.github.com>
2023-05-06 17:40:44 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 const port = 3000;
 
 
@@ -10,6 +12,20 @@ app.get('/', (req, res) => {
   res.send('Hola Mundo!')
 });
 
+app.get('/blog', (req, res) => {
+    res.send('Hola Blog, mi nombre es Mateo!')
+  });
+  
+
 app.listen(port, () => {
   console.log(`La conexion funciona bien en el puerto ${port}`)
 });
+
+
+mongoose
+.connect('mongodb+srv://Matulegui:Epiphonelogitech@cluster0.utjvw.mongodb.net/NODE-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Contecdo a base de datos MONGO')
+}).catch((error) => {
+    console.log(error)
+})
