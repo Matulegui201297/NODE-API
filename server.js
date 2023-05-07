30f35c0c247485254d0e79da444ecad2946e5535
MATU_LEGUI <92628713+Matulegui201297@users.noreply.github.com>
2023-05-07 10:02:59 -0300

    creacion de modelos, Schema y conectado con  la base de datos de Mongo

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// Para conectar - tipear npm run dev
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Producto = require('./models/productModels');
 
 const port = 3000;
+
+app.use(express.json());
 
 
 // ROUTES
@@ -22,10 +27,32 @@ app.listen(port, () => {
 });
 
 
+app.get('/product', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+
+app.post('/product', async (req, res) => {
+   try {
+    const product = await Producto.create(req.body)
+    res.status(200).json(product);
+   } 
+   catch (error) {
+    console.log(error.message)
+    res.status(500).json({message: error.message})
+   }
+})
+
+
 mongoose
 .connect('mongodb+srv://Matulegui:Epiphonelogitech@cluster0.utjvw.mongodb.net/NODE-API?retryWrites=true&w=majority')
 .then(() => {
-    console.log('Contecdo a base de datos MONGO')
+    console.log('Conectado a base de datos MONGO')
 }).catch((error) => {
     console.log(error)
 })
